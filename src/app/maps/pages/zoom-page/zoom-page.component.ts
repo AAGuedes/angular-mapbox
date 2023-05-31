cc8d0dc622bcf3726ab929eb6682df32eb8bfed7
AAGuedes <airam.a.g.85@gmail.com>
2023-05-31 08:46:24 +0100

    Diseño - ZoomIn y ZoomOut

--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-page',
   templateUrl: './zoom-page.component.html',
   styleUrls: ['./zoom-page.component.css']
 })
-export class ZoomPageComponent {
+export class ZoomPageComponent implements AfterViewInit {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw 'Elemento HTML no encontrado';
+
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }
 
 }
