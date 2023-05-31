d69b0aa789e0ca43e9e2908710a7a5bb52448321
AAGuedes <airam.a.g.85@gmail.com>
2023-05-31 09:38:00 +0100

    Obtener las coordenadas centrales del mapa

--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -1,18 +1,19 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-page',
   templateUrl: './zoom-page.component.html',
   styleUrls: ['./zoom-page.component.css']
 })
-export class ZoomPageComponent implements AfterViewInit {
+export class ZoomPageComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('map')
   public divMap?: ElementRef;
 
   public zoom: number = 10;
   public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-3.70256000000000, 40.41650000000000);
 
   ngAfterViewInit(): void {
 
@@ -21,11 +22,15 @@ export class ZoomPageComponent implements AfterViewInit {
     this.map = new Map({
       container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-74.5, 40],
+      center: this.currentLngLat,
       zoom: this.zoom,
     });
 
     this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
   mapListeners() {
@@ -38,6 +43,10 @@ export class ZoomPageComponent implements AfterViewInit {
     this.map!.on('zoomend',(event) => {
       if(this.map!.getZoom() < 18) return;
       this.map!.zoomTo(18);
+    })
+
+    this.map.on('move', (event) => {
+      this.currentLngLat = this.map!.getCenter();
     })
   }
 
