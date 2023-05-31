7cbfce43c01bd011fe3218d7ba85aa57ff3f47ec
AAGuedes <airam.a.g.85@gmail.com>
2023-05-31 10:00:15 +0100

    Marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-markers-page',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./markers-page.component.css']
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public zoom: number = 13;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-3.70256000000000, 40.41650000000000);
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw 'Elemento HTML no encontrado';
+
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.currentLngLat,
+      zoom: this.zoom,
+    });
+
+    const marker = new Marker({
+      color: '#E74C3C'
+    })
+      .setLngLat(this.currentLngLat)
+      .addTo(this.map);
+  }
+
 
 }
