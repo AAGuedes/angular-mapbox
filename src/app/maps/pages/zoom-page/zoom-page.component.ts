e89957c4f504104d1466aa71813602891b732135
AAGuedes <airam.a.g.85@gmail.com>
2023-05-31 09:03:54 +0100

    Controlar el nivel del Zoom

--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -11,16 +11,53 @@ export class ZoomPageComponent implements AfterViewInit {
   @ViewChild('map')
   public divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: Map;
+
   ngAfterViewInit(): void {
 
     if(!this.divMap) throw 'Elemento HTML no encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-74.5, 40],
-      zoom: 9,
+      zoom: this.zoom,
     });
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if(!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (event) => {
+      this.zoom =  this.map!.getZoom();
+    });
+
+    this.map!.on('zoomend',(event) => {
+      if(this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    })
+  }
+
+  zoomIn() {
+    if(!this.map) throw 'Mapa no inicializado';
+
+    this.map.zoomIn();
+  }
+
+  zoomOut() {
+    if(!this.map) throw 'Mapa no inicializado';
+
+    this.map.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    if(!this.map) throw 'Mapa no inicializado';
+
+    this.zoom = Number(value);
+    this.map!.zoomTo(this.zoom);
   }
 
 }
